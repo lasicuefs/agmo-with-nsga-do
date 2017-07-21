5d17f23697bf42e1c9178b5218bd84791398dff6
João Paulo <joaopaulo761@gmail.com>
2017-07-21 10:02:20 -0300

    Changes + JMetal Study Example

---
+++ Project 5.3/Codes/src/main/java/jpssena/experiments/Experiment_Learn_MultiObjective_MGPiresWay.java
@@ -0,0 +1,8 @@
+package jpssena.experiments;
+
+/**
+ * Created by João Paulo on 21/07/2017.
+ */
+public class Experiment_Learn_MultiObjective_MGPiresWay {
+    
+}
---
+++ Project 5.3/Codes/test/ZDTStudy/R/SPREAD.Boxplot.R
@@ -0,0 +1,31 @@
+postscript("SPREAD.Boxplot.eps", horizontal=FALSE, onefile=FALSE, height=8, width=12, pointsize=10)
+resultDirectory<-"../data"
+qIndicator <- function(indicator, problem)
+{
+fileSMPSO<-paste(resultDirectory, "SMPSO", sep="/")
+fileSMPSO<-paste(fileSMPSO, problem, sep="/")
+fileSMPSO<-paste(fileSMPSO, indicator, sep="/")
+SMPSO<-scan(fileSMPSO)
+
+fileNSGAII<-paste(resultDirectory, "NSGAII", sep="/")
+fileNSGAII<-paste(fileNSGAII, problem, sep="/")
+fileNSGAII<-paste(fileNSGAII, indicator, sep="/")
+NSGAII<-scan(fileNSGAII)
+
+fileSPEA2<-paste(resultDirectory, "SPEA2", sep="/")
+fileSPEA2<-paste(fileSPEA2, problem, sep="/")
+fileSPEA2<-paste(fileSPEA2, indicator, sep="/")
+SPEA2<-scan(fileSPEA2)
+
+algs<-c("SMPSO","NSGAII","SPEA2")
+boxplot(SMPSO,NSGAII,SPEA2,names=algs, notch = TRUE)
+titulo <-paste(indicator, problem, sep=":")
+title(main=titulo)
+}
+par(mfrow=c(3,3))
+indicator<-"SPREAD"
+qIndicator(indicator, "ZDT1")
+qIndicator(indicator, "ZDT2")
+qIndicator(indicator, "ZDT3")
+qIndicator(indicator, "ZDT4")
+qIndicator(indicator, "ZDT6")
