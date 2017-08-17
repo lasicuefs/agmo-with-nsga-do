61f26d9ef4bc6ce54160c27e56adb986333a4da4
João Paulo <joaopaulo761@gmail.com>
2017-08-17 16:54:27 -0300

    Comments + SD Fix

--- Project 5.3/Codes/src/main/java/experiment/component/GenerateStatistics.java
+++ Project 5.3/Codes/src/main/java/experiment/component/GenerateStatistics.java
@@ -18,54 +18,86 @@ import java.util.List;
 public class GenerateStatistics<S extends Solution<?>, Result> implements ExperimentComponent {
     private final Experiment<S, Result> experiment;
     private Hashtable<String, List<Hashtable<Integer, ObjectiveStatistics>>> directories;
+    private Hashtable<String, List<Hashtable<Integer, ObjectiveStatistics>>> means;
 
     public GenerateStatistics(Experiment<S, Result> experiment) {
         this.experiment = experiment;
         directories = new Hashtable<>();
+        means = new Hashtable<>();
     }
 
     @Override
     public void run() throws IOException {
+        //For each algorithm
         for (ExperimentAlgorithm<S, Result> experimentAlgorithm : experiment.getAlgorithmList()) {
+            //Go to the Algorithm directory
             String algorithmDirectory = experiment.getExperimentBaseDirectory() + "/data/" + experimentAlgorithm.getAlgorithmTag();
+            //Then the Problem directory
             String problemDirectory = algorithmDirectory + "/" + experimentAlgorithm.getProblemTag();
+            //Get the problem tag
             String problemTag = experimentAlgorithm.getProblemTag().split("-")[0];
+            //And the the problem base directory
             String problemBase = algorithmDirectory + "/" + problemTag;
 
+            //Creates the problem base Directory
             File problemBaseDirectory = new File(problemBase);
             problemBaseDirectory.mkdirs();
 
+            //Create a file for the results of all runs
             File result = new File(problemBase + "/resultsRun.txt");
             result.createNewFile();
 
-
+            //This list contains the mean and sd of every objective of a fold
             List<Hashtable<Integer, ObjectiveStatistics>> hashtableList = new ArrayList<>();
 
+            //For each run of fold
             for (int run = 0; run < experiment.getIndependentRuns(); run++) {
+                //Gets the function file that contains the objectives values
                 File function = new File (problemDirectory + "/" + experiment.getOutputParetoFrontFileName() + run + ".tsv");
-
+                //Split the file to get the values of the objectives [here every single objective has an ID]
                 Hashtable<Integer, List<Double>> objectivesPerValue = GeneticUtil.functionFileToObjectivesValue(function);
+                //Gets the mean and sd of every single objective in this run
                 Hashtable<Integer, ObjectiveStatistics> statistics = createMeanAndSD(objectivesPerValue);
+                //Writes down the mean and SD of this run
                 writeStatistics(experimentAlgorithm.getProblemTag() + " " + run, statistics, result);
+                //Adds this run to the list of fold
                 hashtableList.add(statistics);
+
+                //We store all means and SD into the full thing without the folding thing;
+                List<Hashtable<Integer, ObjectiveStatistics>> all = means.get(problemBase);
+                if (all == null) all = new ArrayList<>();
+                all.add(statistics);
+                means.put(problemBase, all);
             }
 
+            //After the we have the values of mean and sd of every run of a fold
+            //we create a statistic for the hole folding
             Hashtable<Integer, ObjectiveStatistics> objectiveStatistics = createSingleStatistics(hashtableList);
 
+            //Then, we need to add the mean and SD of the fold into the problem execution list
+            //So, first we get the list of folds results
             List<Hashtable<Integer, ObjectiveStatistics>> problemStats = directories.get(problemBase);
 
+            //If the list doesn't exists we create it
             if (problemStats == null)
                 problemStats = new ArrayList<>();
 
+            //Then we add the result of this fold into it
             problemStats.add(objectiveStatistics);
+            //And we put it back into the hash table
             directories.put(problemBase, problemStats);
         }
 
+        //For each problem we solved using this algorithm
         for (String problemBase : directories.keySet()) {
+            //We create a file to store the results
             File resultFile = new File(problemBase + "/result.txt");
             resultFile.createNewFile();
-            List<Hashtable<Integer, ObjectiveStatistics>> problemStats = directories.get(problemBase);
+            //We get the list of the result of every fold
+            List<Hashtable<Integer, ObjectiveStatistics>> problemStats = means.get(problemBase);
+            //Create the Mean and SD using the result of all folds
             Hashtable<Integer, ObjectiveStatistics> statisticsHashtable = createSingleStatistics(problemStats);
+            //Then writes down the result
             writeStatistics("", statisticsHashtable, resultFile);
         }
     }
